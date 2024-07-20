6be320d7325aef96b666139347e50381287f0d0a
Asmit <asmittyagi143@gmail.com>
2024-07-20 18:43:06 +0530

    characterArray

---
+++ DSA -LB/charArray.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+ int main(){
+    char ch[100];
+    cin>>ch;
+    for(int i =0 ; i>7;i++){
+        cout<<"index:"<<i<<"value:"<<ch[i]<<endl;
+
+    }
+    return 0;
+    
+
+
+ }
